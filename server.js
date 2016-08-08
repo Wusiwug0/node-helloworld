e54adbc23a5741bb3d8993bad3c67d425dfb43aa
Wusiwug0 <palagiris.0201@gmail.com>
2016-08-07 20:44:44 -0400

    test

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require("express"),
     app = express();
 
-var port = process.env.VCAP_APP_PORT || 8081;
+var port = process.env.VCAP_APP_PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get("/hello", function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello World!\n");
+    response.end("Helloooo World!\n");
 });
 
-app.listen(port);
+app.listen("0.0.0.0",port);
 
 require("cf-deployment-tracker-client").track();
